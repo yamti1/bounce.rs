95c7ed3da668785e85766260b6cc178b2d2f3c23
yam t <yam.tirosh@gmail.com>
2020-10-08 00:39:46 +0300

    Init commit

---
+++ src/bouncing_ball.rs
@@ -0,0 +1,74 @@
+use rand::prelude::*;
+use ggez::{Context, GameResult, graphics};
+use ggez::nalgebra as na;
+
+use super::{WINDOW_WIDTH, WINDOW_HEIGHT};
+
+type Range = (f32, f32);
+
+const RADIUS_RANGE: Range = (5.0, 200.0);
+const FATNESS_RANGE: Range = (0.1, 1.0);
+const MAX_SPEED: f32 = 10.0;
+const DIRECTIONS: [i8; 2] = [-1, 1];
+
+pub struct BouncingBall {
+    radius: f32,
+    speed_factor: f32,
+    color: graphics::Color,
+    x: f32,
+    y: f32,
+    dx: i8,
+    dy: i8,
+}
+
+impl BouncingBall {
+    pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let fatness_distribution = rand::distributions::Uniform::new_inclusive(FATNESS_RANGE.0, FATNESS_RANGE.1);
+        let fatness = rng.sample(fatness_distribution);
+        Self {
+            radius: fatness * RADIUS_RANGE.1 + RADIUS_RANGE.0,
+            speed_factor: 1.0 - fatness,
+            color: graphics::Color::new(
+                rng.gen::<f32>(), 
+                rng.gen::<f32>(), 
+                rng.gen::<f32>(), 
+                1.0 - fatness
+            ),
+            x: WINDOW_WIDTH / 2.0,
+            y: WINDOW_HEIGHT / 2.0,
+            dx: DIRECTIONS.choose(&mut rng).unwrap().clone(),
+            dy: DIRECTIONS.choose(&mut rng).unwrap().clone(),
+        }
+    }
+
+    pub fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let screen = graphics::screen_coordinates(ctx);
+
+        if self.x + self.radius >= screen.w || self.x - self.radius <= 0.0 {
+            self.dx *= -1;
+        }
+        if self.y + self.radius >= screen.h || self.y - self.radius <= 0.0 {
+            self.dy *= -1;
+        }
+        
+        self.x += self.speed_factor * MAX_SPEED * self.dx as f32;
+        self.y += self.speed_factor * MAX_SPEED * self.dy as f32;
+
+        Ok(())
+    }
+
+    pub fn draw(&self, ctx: &mut Context) -> GameResult {
+        let ball = graphics::Mesh::new_circle(
+            ctx, 
+            graphics::DrawMode::fill(), 
+            na::Point2::new(0.0, 0.0), 
+            self.radius,
+            0.25, 
+            self.color,
+        )?;
+        graphics::draw(ctx, &ball, (na::Point2::new(self.x, self.y),))?;
+
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+mod bouncing_ball;
+
+use ggez::*;
+use bouncing_ball::BouncingBall;
+
+const BALL_COUNT: usize = 10;
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 480.0;
+
+struct State {
+    balls: Vec<BouncingBall>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        let mut balls = Vec::with_capacity(BALL_COUNT);
+        for _ in 0..BALL_COUNT {
+            balls.push(BouncingBall::new());
+        }
+
+        Self {
+            balls
+        }
+    }
+}
+
+impl ggez::event::EventHandler for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        for ball in self.balls.iter_mut() {
+            ball.update(ctx)?;
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+
+        for ball in self.balls.iter() {
+            ball.draw(ctx)?;
+        }
+
+        graphics::present(ctx)?;
+
+        Ok(())
+    }
+}
+
+
+fn main() -> GameResult {
+    let state = &mut State::new();
+
+    let mut c = conf::Conf::new();
+    c.window_mode.width = WINDOW_WIDTH;
+    c.window_mode.height = WINDOW_HEIGHT;
+    c.window_setup.title = String::from("Bouncing Balls");
+
+    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("Bouncing Balls", "yamti")
+        .conf(c).build()?;
+    
+    event::run(ctx, event_loop, state)
+}
